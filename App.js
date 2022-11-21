f6f3debc3a4e61be10178a94bb2a78f76dd1dc6e
LorenThuan <thuan22022001@gmail.com>
2022-11-21 15:12:04 +0700

    fix lib

--- App.js
+++ App.js
@@ -67,7 +67,7 @@ const customTransition = {
   }
 }
 export default function App() {
-  const Stack = createNativeStackNavigator();
+  // const Stack = createStackNavigator();
   return (
     <NavigationContainer>
     <Stack.Navigator screenOptions={{headerShown:false,
